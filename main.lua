ff806f5a48504fa235e4197bb0886932898da413
factwixard <factwixard@gmail.com>
2025-08-01 22:40:39 -0700

    added Anvil

--- main.lua
+++ main.lua
@@ -576,7 +576,76 @@ ExtraEffects = {
         end,
     },
 }
-
+SMODS.Atlas ({
+    key = 'anvil',
+    path = 'anvil.png',
+    px = 71,
+    py = 95,
+})
+SMODS.Consumable {
+    key = 'anvil',
+    set = 'Spectral',
+    atlas = 'anvil',
+    loc_txt = {
+        name = 'Anvil',
+        text = {
+            "Increase a {C:attention}random{} Effect's {C:attention}Potency{}",
+            "on a {C:attention}random{} Joker by {C:attention}#1#%{}",
+            "{C:inactive}(Up to 100%){}",
+        },
+    },
+    config = { extra = { potency = 20 } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.potency } }
+    end,
+    use = function(self, card, area, copier)
+        local joker_pool = {}
+            for i,v in ipairs(G.jokers.cards) do
+                if v.ability.hsr_extra_effects then
+                    for _,vv in ipairs(v.ability.hsr_extra_effects) do
+                        if vv.ability and vv.ability.perfect and vv.ability.perfect < 100 then
+                            joker_pool[#joker_pool+1] = v
+                            break
+                        end
+                    end
+                end
+            end
+        local random_joker = pseudorandom_element(joker_pool, pseudoseed("stck_anvil"))
+        local pool = {}
+        for i,v in ipairs(random_joker.ability.hsr_extra_effects) do
+            if v.ability and v.ability.perfect then pool[#pool+1] = v end
+        end
+        local random = pseudorandom_element(pool, pseudoseed("stck_anvil"))
+        if random and random.ability and random.ability.perfect and random.ability.perfect < 100 then
+            random.ability.perfect = math.min(random.ability.perfect + card.ability.extra.potency, 100)
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.4,
+                func = function()
+                    play_sound('tarot1')
+                    card:juice_up(0.3, 0.5)
+                    return true
+                end
+            }))
+        end
+    end,
+    can_use = function(self, card)
+        local joker_test = false
+            for i,v in ipairs(G.jokers.cards) do
+                if v.ability.hsr_extra_effects then
+                    for _,vv in ipairs(v.ability.hsr_extra_effects) do
+                        if vv.ability and vv.ability.perfect and vv.ability.perfect < 100 then
+                            joker_test = true
+                            break
+                        end
+                    end
+                end
+            end
+        if joker_test == true then
+            return true
+        end
+    end
+}
 local function is_food_joker(joker) --thanks dilly
     if joker.ability.isFood then
         return joker.ability.isFood
